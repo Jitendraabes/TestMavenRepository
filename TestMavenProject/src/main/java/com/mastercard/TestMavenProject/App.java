3cc229cbfbdbf30244ebc5db9d92c5460a2316e5
Jitendra <jitendra.abes@gmail.com>
2019-04-19 15:26:34 +0530

    My first maven hello world java project

---
+++ TestMavenProject/src/main/java/com/mastercard/TestMavenProject/App.java
@@ -0,0 +1,14 @@
+package com.mastercard.TestMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven World!" );
+        System.out.println("Will run maven in Ec2 as well");
+    }
+}
